0e8be5b9dc73fb72ae8d951cdb25f9dc21d2cfb0
Nevin Babu <nevinbabu25@gmail.com>
2026-07-05 22:57:29 +0530

    added-mutation-observer

--- email-writer-ext/content.js
+++ email-writer-ext/content.js
@@ -1,1 +1,21 @@
 console.log("Email Writer extension - content script loaded");
+function injectButton() {
+    
+}
+const observer = new MutationObserver((mutations) => {
+    for(const mutation of mutations) {
+        const addedNotes = Array.from(mutation.addedNodes);
+        const hasComposedElements = addedNotes.some(node => 
+            node.nodeType === Node.ELEMENT_NODE && 
+            (node.matches('.aDg,nH Hd, [role="dialog"]') || node.querySelector('.aDg,nH Hd, [role="dialog"]'))
+        );
+        if (hasComposedElements){
+        console.log("Compose window Detected!");
+        setTimeout(injectButton,500);
+    }
+    }
+});
+observer.observe(document.body,{
+    childList:true,
+    subtree: true
+});
